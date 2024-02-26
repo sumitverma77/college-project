0bf664dfa808f9dd94a30a6b0155524161216a17
Sumit Verma <987sumitverma@gmail.com>
2024-02-26 19:02:33 +0530

    is everything fine?

--- src/main/java/com/example/collegeproject/dto/AuthenticationRequest.java
+++ src/main/java/com/example/collegeproject/dto/AuthenticationRequest.java
@@ -8,6 +8,7 @@ public class AuthenticationRequest {
         private String      password;
 
         // Constructors, getters, and setters
+    //is everything  fine?
 
     public String getUsername() {
         return username;
